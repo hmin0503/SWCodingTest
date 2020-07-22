e8bc3cd2d2def497c51ff3b8250dae3e9d640da5
HyangsukMin <hyangsukmin503@gmail.com>
2020-07-22 22:43:28 +0900

    2020-07-22

--- sw5_10204.py
+++ sw5_10204.py
@@ -1,7 +1,7 @@
 '''
 https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXMCcO16Vi8DFAWv&categoryId=AXMCcO16Vi8DFAWv&categoryType=CODE
 Date : 2020-07-22
-Memo : 제한시간 초과
+Memo :
 '''
 #%%
 T = int(input())
@@ -10,19 +10,16 @@ for test_case in range(1, T+1):
     j_happiness = 0
     h_happiness = 0
     dishes = int(input())
-    j_dishes_happiness = dict()
-    h_dishes_happiness = dict()
-    
+    dishes_happiness = dict()
     for d in range(dishes):
         a,b = map(int,input().split())
-        j_dishes_happiness[d] = [a,b]
-        h_dishes_happiness[d] = [b,a]
-    j_dishes_happiness = {k:v[0] for k,v in sorted(j_dishes_happiness.items(),key=lambda x : (x[1][0],x[1][1]),reverse=True)}
-    h_dishes_happiness = {k:v[0] for k,v in sorted(h_dishes_happiness.items(),key=lambda x : (x[1][0],x[1][1]),reverse=True)}
+        dishes_happiness[d] = [a,b]
+    j_dishes_happiness = {k:v[0] for k,v in sorted(dishes_happiness.items(),key=lambda x : (x[1][0],x[1][1]),reverse=True)}
+    h_dishes_happiness = {k:v[1] for k,v in sorted(dishes_happiness.items(),key=lambda x : (x[1][1],x[1][0]),reverse=True)}
+
     for d in range(dishes):
         if d % 2 == 0 :
             k = next(iter(j_dishes_happiness))
-            print(k)
             j_happiness += j_dishes_happiness[k]
             del j_dishes_happiness[k]
             del h_dishes_happiness[k]
@@ -32,3 +29,11 @@ for test_case in range(1, T+1):
             del j_dishes_happiness[k]
             del h_dishes_happiness[k]
     print(j_happiness - h_happiness)
+
+
+# %%
+
+# %%
+
+
+# %%
