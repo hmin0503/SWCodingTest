81f0e584413ca08da85ddc16b135182befddf8cd
hmin0503 <hyangsukmin503@gmail.com>
2022-10-13 11:31:51 +0900

    BOJ SWEA

---
+++ Algorithms/BOJ_Implementation_G4_14502_BFS-BruteForce.py
@@ -0,0 +1,13 @@
+#------------------------
+# #Implementation #Simulation #BruteForce #BFS #pypy3
+# https://www.acmicpc.net/problem/14502
+#------------------------
+
+def dfs(r, c):
+    if 0 <= r < N and 0 <= c < N:
+        return False
+    
+if __name__ ==' __main__':
+    N, M = map(int, input().split())
+    maps = [list(map(int, input().split())) for _ in range(N)]
+
