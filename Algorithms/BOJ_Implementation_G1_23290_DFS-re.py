c6c6356af35394c8a2b3677f09c60beb64920b83
hmin0503 <hyangsukmin503@gmail.com>
2022-10-14 17:05:58 +0900

    BOJ SWEA

---
+++ Algorithms/BOJ_Implementation_G1_23290_DFS-re.py
@@ -0,0 +1,120 @@
+#------------------------
+# #Implementation #Simulation #DFS #다른사람풀이
+# https://www.acmicpc.net/problem/23290
+#------------------------
+
+def moveFish():
+    res = [[[] for _ in range(5)] for _ in range(5)]
+    for r in range(1, 5):
+        for c in range(1, 5):
+            while copyFishLocs[r][c]:
+                d = copyFishLocs[r][c].pop()
+                # for i in range(8):
+                    # nd = (d-i)%8
+                for nd in range(d, d-8, -1):
+                    nd %= 8
+                    nr, nc = r + ddr[nd], c + ddc[nd]
+                    # if s == 3:
+                    #     print(r,c,d, "->", nr, nc, nd, "|", fishSmell[nr][nc])
+                    if (nr, nc) != (sr,sc) and \
+                         0 < nr < 5 and 0 < nc < 5 and \
+                            fishSmell[nr][nc] == 0:
+
+                        res[nr][nc].append(nd)
+                        break
+                else:
+                    res[r][c].append(d)
+
+    return res
+
+def moveShark(r, c, dep, cnt, visit):
+    global maximum, sr, sc, eaten
+    if dep == 3:
+        if maximum < cnt:
+            maximum = cnt
+            sr, sc = r, c
+            eaten = visit[:]
+        return
+    for d in range(4):
+        nr = r + dr[d]
+        nc = c + dc[d]
+        if 0 < nr < 5 and 0 < nc < 5:
+            # 방문 안한 곳이면 방문 표시 후 다음으로.
+            if (nr, nc) not in visit:
+                visit.append((nr,nc))
+                moveShark(nr, nc, dep + 1, cnt + len(copyFishLocs[nr][nc]), visit)
+                visit.pop()
+            # 방문 한 곳이면 깊이만 추가 후 이동.
+            else:
+                moveShark(nr, nc, dep + 1, cnt, visit)
+
+from copy import deepcopy
+if __name__ == "__main__":
+    # 0,  1, 2, 3, 4,  5,  6, 7
+    # ←, ↖, ↑, ↗, →, ↘, ↓, ↙
+    ddr = [0, -1, -1, -1, 0, 1, 1, 1]
+    ddc = [-1, -1, 0, 1, 1, 1, 0, -1]    
+
+    dr = [-1, 0, 1, 0]
+    dc = [0, -1, 0, 1]
+
+    M, S = map(int, input().split())
+    fish = [list(map(int, input().split())) for _ in range(M)]
+    fishLocs = [[[] for _ in range(5)] for _ in range(5)]
+
+    for r, c, d in fish:
+        fishLocs[r][c].append(d-1)
+
+    sr, sc = map(int, input().split())
+
+    fishSmell = [[0] * 5 for _ in range(5)]
+
+    for s in range(1, S+1):
+        # 물고기 복제.
+        # print("shakr", sr, sc)
+        copyFishLocs = deepcopy(fishLocs)
+        # print("initial fish", s)
+        # for row in copyFishLocs[1:]: print(*row[1:])
+
+        # 물고기 이동.
+        copyFishLocs = moveFish()
+        # print("move fish", s)
+        # for row in copyFishLocs[1:]: print(*row[1:])
+        # print("fish smell", s)
+        # for row in fishSmell[1:]: print(*row[1:])
+
+        # print()
+        # 상어 이동.
+        eaten = []
+        maximum = -1e9 
+        moveShark(sr, sc, 0, 0, [])
+        # 상어 이동 후 물고기 제거.
+        # print("eaten", eaten)
+        # print()
+        for r, c in eaten:
+            if copyFishLocs[r][c]:
+                copyFishLocs[r][c] = []
+                fishSmell[r][c] = 3
+        
+        # 물고기 냄새 제거.
+        for r in range(1, 5):
+            for c in range(1, 5):
+                if fishSmell[r][c] > 0:
+                    fishSmell[r][c] -= 1
+
+        # 물고기 복제.
+        for r in range(1, 5):
+            for c in range(1, 5):
+                fishLocs[r][c] += copyFishLocs[r][c]
+        
+        # print("final", s)
+        # for row in copyFishLocs[1:]: print(*row[1:])
+        # print()
+
+    answers = 0
+    for r in range(1, 5):
+        for c in range(1, 5):
+            answers += len(fishLocs[r][c])
+
+    print(answers)
+
--- Algorithms/BOJ_Implementation_G1_23290_DFS.py
+++ Algorithms/BOJ_Implementation_G1_23290_DFS.py
@@ -3,76 +3,119 @@
 # https://www.acmicpc.net/problem/23290
 #------------------------
 
-
 def moveFish(fish):
+    # 0,  1, 2, 3, 4,  5,  6, 7
+    # ←, ↖, ↑, ↗, →, ↘, ↓, ↙
     dr = [0, -1, -1, -1, 0, 1, 1, 1]
     dc = [-1, -1, 0, 1, 1, 1, 0, -1]
-    fish = deque(fish)
-    for i in range(len(fish)):
-        r, c, d = fish.popleft()
-        nd = d
-        while True:
-            nr = r + dr[nd]
-            nc = c + dc[nd]
-            # 이동 후 범위 안에 있는가? 상어가 없는가? 또는 물고기 냄새가 없는가?
-            if 0 <= nr < 4 and 0 <= nc < 4 and maps[nr][nc] == 0:
-                fishMaps[r][c] -= 1
-                fishMaps[nr][nc] += 1
-                fish.append((nr,nc,d))
-            else:
-                nd = (d-1)%8
-            if d == nd:
-                break
-    return fish
-
-def moveShark(r, c):
-    dr = [-1, 0, 1, 0]
-    dc = [0, 1, 0, -1]
-
-    # 상어 방향 찾기.
-    answers = -1e9
-    for i in range(4):
-        kill = 0
-        nr = r + dr[i]*3
-        nc = c + dc[i]*3
-        #  이동 가능 한가?
-        if 0 <= nr < 4 and 0 <= nc < 4:
-            for j in range(3):
-                nr = r + dr[j]
-                nc = c + dc[j]
-                kill += fishMaps[nr][nc]
-        if answers < kill :
-            answer = kill
-            d = i
+    newFish = {}
     
-    # 상어 이동하기
-    for i in range(3):
-        nr = r + dr[d]
-        nc = r + dc[d]
-        fishMaps[nr][nc] = 0
-        maps[nr][nc] = 2
+    for k, v in fish.items():
+        for r, c, d in v:
+            nd = d
+            while True:
+                nr = r + dr[nd]
+                nc = c + dc[nd]
+                # 이동 후 범위 안에 있는가? 상어가 없는가? 또는 물고기 냄새가 없는가?
+                if 0 < nr < 5 and 0 < nc < 5 and maps[nr][nc] == 0:
+                    if (nr, nc) in newFish:
+                        newFish[(nr,nc)].append((nr,nc,nd))
+                    else:
+                        newFish[(nr,nc)] = [(nr,nc,nd)]
+                    break
+                else:
+                    # 회전 후 방향 제대로 넣기...!
+                    nd = (nd-1)%8
+                    if d == nd:
+                        if (r,c) in newFish:
+                            newFish[(r,c)].append((r,c,d))
+                        else:
+                            newFish[(r,c)] = [(r,c,d)]
+                        break
+    return newFish
 
+def findCandidate(move, sr, sc):
+    if len(move) == 3:
+        moves.append(move[:]) # 리스트 아예 복제해서 넣어야 함...
+        return
+    # 상어 방향 찾기.
+    for i in range(4):
+        nr = sr + dr[i]
+        nc = sc + dc[i]
+        #  이동 가능 한가?
+        if 0 < nr < 5 and 0 < nc < 5:
+            move.append(i)
+            findCandidate(move, nr, nc)
+            move.pop()
 
+def findMaximum(fish, moves, sr, sc, s):
+    maximum = -1e9
+    for move in moves:
+        total = 0
+        r, c = sr, sc
+        tmpFish = deepcopy(fish)
+        for d in move:
+            r += dr[d]
+            c += dc[d]
+            # 물고기를 중복으로 잡지 않도록하기.
+            if (r,c) in tmpFish:
+                total += len(tmpFish[(r,c)])
+                tmpFish[(r,c)] = []
+        if maximum < total :
+            maximum = total
+            final = move
+    for d in final:
+        sr += dr[d]
+        sc += dc[d]
+        if (sr, sc) in fish: 
+            fish[(sr,sc)] = []
+            maps[sr][sc] = s
+    return fish, maps, sr, sc
 
+def deleteFishScent(maps, s):
+    for r in range(1, 5):
+        for c in range(1, 5):
+            if maps[r][c] == s-2:
+                maps[r][c] = 0
+    return maps
 
 from copy import deepcopy
-from collections import deque
 
 if __name__ == '__main__':
     M, S = map(int, input().split())
     # 상어:1, 물고기냄새:2
-    maps = [[0]*4 for _ in range(4)]
-    fishMaps = [[0]*4 for _ in range(4)]
-    fish = []
+    maps = [[0]*5 for _ in range(5)]
+    fish = {}
     for _ in range(M):
         r, c, d = map(int, input().split())
         d -= 1
-        fish.append((r,c,d))
-        fishMaps[r][c] += 1
-    sr, sc = map(int, input().split())
-    maps[sr][sc] = 1
+        if (r,c) in fish:
+            fish[(r,c)].append((r,c,d))
+        else:
+            fish[(r,c)] = [(r,c,d)]
 
-    for _ in range(S):
+    sr, sc = map(int, input().split())
+    dr = [-1, 0, 1, 0]
+    dc = [0, -1, 0, 1]
+
+    for s in range(1, S+1):
+        # 1. 물고기 복제
         copyFish = deepcopy(fish)
+        # 2. 물고기 한 칸 이동.
         fish = moveFish(fish)
-        
+        # 3. 상어 연속 3칸 이동.
+        moves = []
+        findCandidate([], sr, sc)
+        fish, maps, sr, sc = findMaximum(fish, moves, sr, sc, s)
+        # 4. 물고기 냄새 제거.
+        maps = deleteFishScent(maps, s)
+        # 5. 물고기 복제 완료.
+        for k, v in copyFish.items():
+            if k in fish:
+                fish[k].extend(v)
+            else:
+                fish[k] = v
+    answers = 0
+    for k, v in fish.items():
+        answers += len(v)
+    print(answers)
